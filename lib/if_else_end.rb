0ebc6390a104b8d38f1a0a0feb6ce41ea906556f
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-09 17:21:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,14 @@
-# Write your solution here
+
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+
+run_code_inside = false
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"

3fa7ed99e36b9bf96fc5808bada6336793ec6114
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-09 17:22:46 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,14 +1,8 @@
-
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
 end
-puts "Code after if...end"
-
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
